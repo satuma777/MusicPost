c58ee3ca18d13b891d8be1be9bd9601e5eda8e85
terashin777 <terashin0324@gmail.com>
2016-09-13 19:08:30 +0900

    修正#1

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,14 +1,5 @@
 class Sound < ActiveRecord::Base
     #mount_uploader :sound, SoundUploader
-    validates :name, presence: true
+    validates :title, presence: true
     validates :content, presence: true
-    validates :content_type_check
-    validates :content_type_check
-
-    def content_type_check
-      if date.present? && date < Date.today
-        #↑nil, "", " "(半角スペースのみ), [](空の配列), {}(空のハッシュ) のときにfalseを返します。
-        errors.add(:date, ": 過去の日付は使用できません")
-      end
-    end
 end
