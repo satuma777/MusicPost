d000c4367cfea187b0e028b6eb15905b420f1d85
terashin777 <terashin0324@gmail.com>
2016-09-17 18:58:20 +0900

    一時保存

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -29,21 +29,33 @@ class SoundsController < ApplicationController
     def create
         @sound = Sound.new(sound_params)
         file = params[:sound][:upfile]
+        file_img = params[:sound][:image]
         #↑params[:upfile]でもよい。params[:sound]とparamsは同じ見てよい。
         #↑その他の値、例えばidを取ってきたい時は、id = params[:sound][:id]（または、id = params[:id]）とする。
         perms = ['.mp3', '.ogg', '.wav']
+        perms_img = ['.jpg', '.jpeg', '.gif', '.png']
         if !file.nil?
-            file_orgname = file.original_filename
+            file_org = file.original_filename
+            file_org_img = file_img.original_filename
             #↓downcaseメソッドは、文字列中の大文字を小文字に変えた新しい文字列を返す。
             #↓.extname(filename)はファイル名 filename の拡張子部分(最後の "." に続く文字列)を 返す。
             #↓include?メソッドは、文字列の中に引数の文字列が含まれるかどうかを調べる。
-            if !perms.include?(File.extname(file_orgname).downcase) then
+            if !perms.include?(File.extname(file_org).downcase) then
                @sound.upfile = "ext_error"
-            elsif MimeMagic.by_magic(file) != "audio/mp3" && MimeMagic.by_magic(file) != "audio/mpeg" && MimeMagic.by_magic(file) != "audio/wav" && MimeMagic.by_magic(file) != "audio/x-wav" && MimeMagic.by_magic(file) != "audio/ogg" && MimeMagic.by_magic(file) != "video/ogg" && MimeMagic.by_magic(file) != "audio/mpeg" then
+            elsif MimeMagic.by_magic(file) != "audio/mp3" && MimeMagic.by_magic(file) != "audio/mpeg" && MimeMagic.by_magic(file) != "audio/wav" && MimeMagic.by_magic(file) != "audio/x-wav" && MimeMagic.by_magic(file) != "audio/ogg" && MimeMagic.by_magic(file) != "video/ogg" then
                 @sound.upfile = "file_error"
             elsif file.size > 15.megabyte then
                 @sound.upfile = "size_error"
             end
+            #↑音声ファイルのチェック。
+            if !perms_img.include?(File.extname(file_org_img).downcase) then
+               @sound.image = "ext_error"
+            elsif MimeMagic.by_magic(file_img) != "image/jpg" && MimeMagic.by_magic(file_img) != "image/jpeg" && MimeMagic.by_magic(file_img) != "image/png" && MimeMagic.by_magic(file_img) != "image/x-citrix-png" && MimeMagic.by_magic(file_img) != "image/x-citrix-jpeg" && MimeMagic.by_magic(file_img) != "image/x-png" && MimeMagic.by_magic(file_img) != "image/pjpeg" then
+                @sound.image = "file_error"
+            elsif file_img.size > 1.megabyte then
+                @sound.image = "size_error"
+            end
+            #↑サムネイルとなる画像ファイルのチェック。
             unless @sound.valid?
                 render :new
             else
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -5,27 +5,28 @@ class Sound < ActiveRecord::Base
     validates :title, presence: true
     validates :content, presence: true
     validate :check_sound
+    validate :check_image
 
      def check_sound
         if upfile != nil
             if upfile == "ext_error" then
                 errors[:base] << "投稿できるのは、mp3、ogg、wavのみです。"
             elsif upfile == "file_error" then
-                 errors[:base] << "不正なファイルです。"
+                 errors[:base] << "不正なファイル形式です。"
             elsif upfile == "size_error" then
                 errors[:base] << "ファイルサイズは15MBまでです。"
             end
         end
     end
 
-    def check_sound
-        if upfile != nil
-            if upfile == "ext_error" then
-                errors[:base] << "投稿できるのは、mp3、ogg、wavのみです。"
-            elsif upfile == "file_error" then
-                 errors[:base] << "不正なファイルです。"
-            elsif upfile == "size_error" then
-                errors[:base] << "ファイルサイズは15MBまでです。"
+    def check_image
+        if image != nil
+            if image == "ext_error" then
+                errors[:base] << "投稿できるのは、jpg、jpeg、gif、pngのみです。"
+            elsif image == "file_error" then
+                 errors[:base] << "不正なファイル形式です。"
+            elsif image == "size_error" then
+                errors[:base] << "ファイルサイズは1MBまでです。"
             end
         end
     end
@@ -34,6 +35,22 @@ class Sound < ActiveRecord::Base
             file_orgname = file.original_filename
             #↑コントローラー側で定義されているfile_orgnameとは別物。
             file_orgname = file_orgname.kconv(Kconv::SJIS, Kconv::UTF8)
+            #file_name = SecureRandom.hex(10) + self.id.to_s
+            file_name = 'sid' + self.object_id.to_s
+            #↑idを取得するときは、.idではなく、.object_idと書く。to_sで文字列（string型）に直している。
+            #↑.object_idは各オブジェクトに対して一意な整数を返す。オブジェクトとは、インスタンスもクラスも含めた一つ一つのものである。
+            #↑インスタンスもオブジェクトなので、オブジェクト1つ1つに対しても一意なidが返される。
+            puts('ファイル名'+file_name)
+            File.open("app/assets/sounds/upfiles/#{file_name}", 'wb') { |f| f.write(file.read) }
+            self.upfile = file_orgname
+            self.path = file_name
+            self.ext_name = File.extname(file_orgname).downcase
+            #↑HTMLでの再生の際は、pathとext_nameを組み合わせて、～.mp3のような名前にし、再生できる形にする。
+    end
+    def set_image(file)
+            file_orgname = file.original_filename
+            #↑コントローラー側で定義されているfile_orgnameとは別物。
+            file_orgname = file_org.kconv(Kconv::SJIS, Kconv::UTF8)
             #file_name = SecureRandom.hex(10) + self.id.to_s
             file_name = 'sid' + self.object_id.to_s
             #↑idを取得するときは、.idではなく、.object_idと書く。to_sで文字列（string型）に直している。
