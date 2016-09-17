803adcf7ca98efc5198a963024fec6d9daddd0c0
terashin777 <terashin0324@gmail.com>
2016-09-17 19:17:02 +0900

    保存

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -36,12 +36,11 @@ class Sound < ActiveRecord::Base
             #↑コントローラー側で定義されているfile_orgnameとは別物。
             file_orgname = file_orgname.kconv(Kconv::SJIS, Kconv::UTF8)
             #file_name = SecureRandom.hex(10) + self.id.to_s
-            file_name = 'sid' + self.object_id.to_s
+            file_name = 'sid' + self.object_id.to_s + File.extname(file_orgname).downcase
             #↑idを取得するときは、.idではなく、.object_idと書く。to_sで文字列（string型）に直している。
             #↑.object_idは各オブジェクトに対して一意な整数を返す。オブジェクトとは、インスタンスもクラスも含めた一つ一つのものである。
             #↑インスタンスもオブジェクトなので、オブジェクト1つ1つに対しても一意なidが返される。
-            puts('ファイル名'+file_name)
-            File.open("app/assets/sounds/upfiles/#{file_name}", 'wb') { |f| f.write(file.read) }
+            File.open("public/uploads/sounds/sound/#{file_name}", 'wb') { |f| f.write(file.read) }
             self.upfile = file_orgname
             self.path = file_name
             self.ext_name = File.extname(file_orgname).downcase
@@ -52,14 +51,12 @@ class Sound < ActiveRecord::Base
             #↑コントローラー側で定義されているfile_orgnameとは別物。
             file_orgname = file_org.kconv(Kconv::SJIS, Kconv::UTF8)
             #file_name = SecureRandom.hex(10) + self.id.to_s
-            file_name = 'sid' + self.object_id.to_s
+            file_name = 'sid' + self.object_id.to_s + File.extname(file_orgname).downcase
             #↑idを取得するときは、.idではなく、.object_idと書く。to_sで文字列（string型）に直している。
             #↑.object_idは各オブジェクトに対して一意な整数を返す。オブジェクトとは、インスタンスもクラスも含めた一つ一つのものである。
             #↑インスタンスもオブジェクトなので、オブジェクト1つ1つに対しても一意なidが返される。
-            puts('ファイル名'+file_name)
-            File.open("app/assets/sounds/upfiles/#{file_name}", 'wb') { |f| f.write(file.read) }
-            self.upfile = file_orgname
-            self.path = file_name
+            File.open("public/uploads/sounds/image/#{file_name}", 'wb') { |f| f.write(file.read) }
+            self.sound = file_orgname
             self.ext_name = File.extname(file_orgname).downcase
             #↑HTMLでの再生の際は、pathとext_nameを組み合わせて、～.mp3のような名前にし、再生できる形にする。
     end
