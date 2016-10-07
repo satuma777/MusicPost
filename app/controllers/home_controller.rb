06510d4794ac508f82ed748bae688200f5c23d87
terashin777 <terashin0324@gmail.com>
2016-10-07 22:09:44 +0900

    注釈#追加；formから受け取るカラムを編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
         @thumb_value = Settings.NEW_THUMB_VALUE
         @sound = Sound.new
         @sounds = Sound.all.order(created_at: :desc)
+        #↑orderはcreated_atによって降順（desc）で並べ替えるようにしている。
     end
 
     def about
--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -166,7 +166,7 @@ class SoundsController < ApplicationController
 
         # Never trust parameters from the scary internet, only allow the white list through.
         def sound_params
-            params.require(:sound).permit(:title, :content, :user_id)
+            params.require(:sound).permit(:title, :content)
         end
 
         def check_sound(sound_file, sound_perms, sound_org_name)
